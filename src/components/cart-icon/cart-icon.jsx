c80300758c9bfe5a50c86871ebd03ce7d4c043a5
dnabaltimore <75803481+dnabaltimore@users.noreply.github.com>
2022-07-02 20:50:58 -0400

    adding cart quantity functions and number to the shopping bag icon

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,21 +1,21 @@
 import './cart-icon.scss';
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
-import {useContext} from 'react';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
 const CartIcon = () => {
-    const {setCartOpen, cartOpen} = useContext(CartContext);
-    const handleCartClick = () => {
-        setCartOpen(!cartOpen);
-    }
+  const { setCartOpen, cartOpen, cartItems, cartQuantity } =
+    useContext(CartContext);
+  const handleCartClick = () => {
+    setCartOpen(!cartOpen);
+  };
 
-    
-    return (
-        <div className='cart-icon-container' onClick={handleCartClick}>
-            <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
-        </div>
-    )
-}
+  return (
+    <div className='cart-icon-container' onClick={handleCartClick}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{cartQuantity}</span>
+    </div>
+  );
+};
 
-export default CartIcon
+export default CartIcon;
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,10 +1,12 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const CartContext = createContext({
   cartItems: [],
   addItemToCart: () => null,
   cartOpen: null,
   setCartOpen: () => null,
+  cartQuantity: 0,
+  setCartQuantity: () => null,
 });
 
 const addItem = (cartItems, productToAdd) => {
@@ -23,15 +25,40 @@ const addItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const getInitialQuantity = (cartItems) => {
+  let count = 0;
+  for (var i = 0; i < cartItems.length; i++) {
+    count += cartItems[i].quantity;
+  }
+  return count;
+};
+
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
   const [cartOpen, setCartOpen] = useState(false);
+  const [cartQuantity, setCartQuantity] = useState(0);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addItem(cartItems, productToAdd));
+    //setCartQuantity(cartQuantity + 1);
   };
 
-  const value = { cartItems, addItemToCart, cartOpen, setCartOpen };
+  useEffect(() => {
+    let count = 0;
+    for (var i = 0; i < cartItems.length; i++) {
+      count += cartItems[i].quantity;
+    }
+    setCartQuantity(count);
+  }, [cartItems]);
+
+  const value = {
+    cartItems,
+    addItemToCart,
+    cartOpen,
+    setCartOpen,
+    cartQuantity,
+    setCartQuantity,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
