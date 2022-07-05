5a0fc43a2ab6f91b87da7986c9cc6a3845d9fbad
dnabaltimore <75803481+dnabaltimore@users.noreply.github.com>
2022-07-05 00:32:00 -0400

    checking out user reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,26 +1,55 @@
-import {createContext, useState, useEffect} from 'react';
-import { onAuthStateChangedListener, createUserDocument } from '../utils/firebase';
+import { createContext, useState, useEffect, useReducer } from 'react';
+import {
+  onAuthStateChangedListener,
+  createUserDocument,
+} from '../utils/firebase';
 
 export const UserContext = createContext({
-    currentUser: null,
-    setCurrentUser: () => null,
+  currentUser: null,
+  setCurrentUser: () => null,
 });
 
-export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = {currentUser, setCurrentUser};
+export const USER_ACTION_TYPE = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChangedListener((user) => {
-            if(user){
-                createUserDocument(user);
-            }
-            setCurrentUser(user);
-            
-        })
-        
-        return unsubscribe;
-    }, [])
+const userReducer = (state, action) => {
+  const { type, payload } = action;
 
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
+  switch (type) {
+    case USER_ACTION_TYPE.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in user reducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+export const UserProvider = ({ children }) => {
+  //const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPE.SET_CURRENT_USER, payload: user });
+  };
+
+  const value = { currentUser, setCurrentUser };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocument(user);
+      }
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
